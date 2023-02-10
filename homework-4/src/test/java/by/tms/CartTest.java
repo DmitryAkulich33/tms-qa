99086c51ac27aec981f46c813aa0cd5c344feba9
Dzmitry Akulich <dzmitry.akulich.33@gmail.com>
2023-02-10 20:11:06 +0300

    add simple test to check cart item

--- homework-4/src/test/java/by/tms/CartTest.java
+++ homework-4/src/test/java/by/tms/CartTest.java
@@ -1,22 +1,58 @@
 package by.tms;
 
+import by.tms.util.Steps;
 import org.openqa.selenium.By;
-import org.testng.Assert;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
+import static by.tms.constants.Credentials.PASSWORD;
+import static by.tms.constants.Credentials.STANDARD_USER;
+import static org.testng.Assert.assertEquals;
+
 public class CartTest extends BaseTest {
+    private final String PRODUCT_NAME_1 = "Sauce Labs Backpack";
+    private final String BUTTON_TEMPLATE = "//div[text()='%s']/ancestor::div[@class='inventory_item']" +
+            "//button[text()='%s']";
+    private final String PRICE_TEMPLATE = "//div[text()='%s']/ancestor::div[@class='inventory_item']" +
+            "//div[@class='inventory_item_price']";
+    private final String ADD_BUTTON = "Add to cart";
+    private final String REMOVE_BUTTON = "Remove";
+    private final String CART_ITEM_TEMPLATE = "//div[text()='%s']/ancestor::div[@class='cart_item']";
 
     @Test
     public void validateProductDescriptionInTheCart() {
+        Steps.submitLogin(webDriver, STANDARD_USER, PASSWORD);
         webDriver.get("https://www.saucedemo.com/inventory.html");
-        String productName = "Sauce Labs Backpack"; // todo private final
-        String addToCartButtonPattern = String.format("//div[text()='%s']/ancestor::div[@class='inventory_item']" +
-                "//button[text()='Add to cart']", productName);
-        webDriver.findElement(By.xpath(addToCartButtonPattern)).click(); // провалидировать что кнопка стала ремув
-        webDriver.findElement(By.id("shopping_cart_container")).click();
 
-        String productInTheCart = String.format("//div[text()='%s']/ancestor::div[@class='cart_item']", productName);
+        List<WebElement> addButtonList = webDriver.findElements(By.xpath(String.format(BUTTON_TEMPLATE, PRODUCT_NAME_1, ADD_BUTTON)));
+        assertEquals(addButtonList.size(), 1);
 
-        Assert.assertTrue(webDriver.findElement(By.xpath(productInTheCart)).isDisplayed(), "Product has not been added to the cart");
+        List<WebElement> priceList = webDriver.findElements(By.xpath(String.format(PRICE_TEMPLATE, PRODUCT_NAME_1)));
+        assertEquals(priceList.size(), 1);
+        String price = priceList.get(0).getText();
+
+        addButtonList.get(0).click();
+        List<WebElement> removeButtonList = webDriver.findElements(By.xpath(String.format(BUTTON_TEMPLATE,
+                PRODUCT_NAME_1, REMOVE_BUTTON)));
+        assertEquals(removeButtonList.size(), 1);
+
+        List<WebElement> cartList = webDriver.findElements(By.id("shopping_cart_container"));
+        assertEquals(cartList.size(), 1);
+        cartList.get(0).click();
+
+        List<WebElement> cartItemList = webDriver.findElements(By.xpath(String.format(CART_ITEM_TEMPLATE, PRODUCT_NAME_1)));
+        assertEquals(cartItemList.size(), 1);
+
+        List<WebElement> itemPriceList = webDriver.findElements(By.xpath(String.format(CART_ITEM_TEMPLATE +
+                "//div[@class='inventory_item_price']", PRODUCT_NAME_1)));
+        assertEquals(itemPriceList.size(), 1);
+        assertEquals(price, itemPriceList.get(0).getText());
+
+        List<WebElement> removeFromCart = webDriver.findElements(By.xpath(String.format(CART_ITEM_TEMPLATE +
+                "//button[text()='%s']", PRODUCT_NAME_1, REMOVE_BUTTON)));
+        assertEquals(removeFromCart.size(), 1);
+        removeFromCart.get(0).click();
     }
 }
--- homework-4/src/test/java/by/tms/util/Steps.java
+++ homework-4/src/test/java/by/tms/util/Steps.java
@@ -3,8 +3,8 @@ package by.tms.util;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class TestUtil {
-    public void submitLogin(WebDriver webDriver, String login, String password) {
+public class Steps {
+    public static void submitLogin(WebDriver webDriver, String login, String password) {
         webDriver.get("https://www.saucedemo.com/");
         webDriver.findElement(By.id("user-name")).sendKeys(login);
         webDriver.findElement(By.id("password")).sendKeys(password);
